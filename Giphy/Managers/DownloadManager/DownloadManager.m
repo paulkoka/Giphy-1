20324e4e2afba577a409feb4eb12a84d0a093dfe
Victor Semenchuk <semenchuk.vitk@gmail.com>
2018-08-23 18:10:38 +0300

    Added fetching data by URL feature

--- Giphy/Managers/DownloadManager/DownloadManager.m
+++ Giphy/Managers/DownloadManager/DownloadManager.m
@@ -10,4 +10,15 @@
 
 @implementation DownloadManager
 
+- (void)fetchDataFromURL:(NSString *)stringUrl withCompletionBlock:(void (^)(NSData *))completionBlock {
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_async(queue, ^{
+        NSURL *url = [NSURL URLWithString:stringUrl];
+        NSData *data = [NSData dataWithContentsOfURL:url];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            completionBlock(data);
+        });
+    });
+}
+
 @end
