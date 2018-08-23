cf6e83945e4e56d919fb777d22e933a562725f85
Pavel Koka <pavelkoka@gmail.com>
2018-08-24 00:50:19 +0300

    url session and request used

--- Giphy/Managers/DownloadManager/DownloadManager.m
+++ Giphy/Managers/DownloadManager/DownloadManager.m
@@ -11,14 +11,23 @@
 @implementation DownloadManager
 
 - (void)fetchDataFromURL:(NSString *)stringUrl withCompletionBlock:(void (^)(NSData *))completionBlock {
-    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-    dispatch_async(queue, ^{
-        NSURL *url = [NSURL URLWithString:stringUrl];
-        NSData *data = [NSData dataWithContentsOfURL:url];
-        dispatch_async(dispatch_get_main_queue(), ^{
+    
+    NSURLRequest* request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:stringUrl] cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:20];
+    
+
+    NSURLSessionConfiguration* configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    configuration.waitsForConnectivity = YES;
+    configuration.allowsCellularAccess = YES;
+    
+    NSURLSession* session = [NSURLSession sessionWithConfiguration: configuration];
+    NSURLSessionDataTask* task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        dispatch_async(dispatch_get_global_queue(QOS_CLASS_UTILITY, 0), ^{
             completionBlock(data);
         });
-    });
+    }];
+    
+    [task resume];
+    
 }
 
 @end
