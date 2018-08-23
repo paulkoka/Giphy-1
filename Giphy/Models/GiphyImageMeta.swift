6f6bc4b0f0cc90c0490e48bf825b3cac7ba216fe
Victor Semenchuk <semenchuk.vitk@gmail.com>
2018-08-23 18:25:59 +0300

    Added models

---
+++ Giphy/Models/GiphyImageMeta.swift
@@ -0,0 +1,14 @@
+//
+//  GiphyImageMeta.swift
+//  Giphy
+//
+//  Created by Viktar Semianchuk on 8/23/18.
+//  Copyright © 2018 Viktar Semianchuk. All rights reserved.
+//
+
+import Foundation
+
+struct GiphyImageMeta: Codable {
+    let url: String?
+    let size: String?
+}
